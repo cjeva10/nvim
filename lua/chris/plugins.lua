06a2d0d8e84af16df2afe0584cf9e182d13ca528
cjeva10 <cjevanko@gmail.com>
2022-11-30 09:03:14 -0500

    Added comment, treesitter, gitsigns

--- lua/chris/plugins.lua
+++ lua/chris/plugins.lua
@@ -40,7 +40,6 @@ packer.init {
 
 -- Install your plugins here
 return packer.startup(function(use)
-    -- My plugins here
     use "wbthomason/packer.nvim" -- Have packer manage itself
     use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
     use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
@@ -75,10 +74,10 @@ return packer.startup(function(use)
     use "kyazdani42/nvim-tree.lua"
 
     -- Treesitter
-    --use {
-    --    "nvim-treesitter/nvim-treesitter",
-    --    run = ":TSUpdate",
-    --}
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+    }
 
     -- Bufferline
     use "akinsho/bufferline.nvim"
@@ -89,6 +88,12 @@ return packer.startup(function(use)
 
     -- Whichkey
     use "folke/which-key.nvim"
+
+    -- gitsigns
+    use "lewis6991/gitsigns.nvim"
+
+    -- comment
+    use "numToStr/Comment.nvim"
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
