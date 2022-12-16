28087bdae140554f419d9367e430ecfda53479be
cjeva10 <cjevanko@gmail.com>
2022-12-16 11:40:38 -0500

    Added neovim/ executable directory to .gitignore

--- lua/chris/treesitter.lua
+++ lua/chris/treesitter.lua
@@ -1,12 +1,12 @@
 local configs = require("nvim-treesitter.configs")
 configs.setup {
-  ensure_installed = "all",
+  ensure_installed = { "help", "lua", "python", "rust", "c", "javascript", "typescript", "solidity" },
   sync_install = false,
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
+    additional_vim_regex_highlighting = false,
 
   },
   indent = { enable = true, disable = { "yaml" } },
