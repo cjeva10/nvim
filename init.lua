d905f5257f48a1a0f4bc0bdb04c75e4a534f89d5
ceva77 <cevanko@binnacle.org>
2023-05-08 11:48:12 -0400

    added comments to init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
+-- load options and keymaps before plugins
 require("chris.options")
 require("chris.keymaps")
+
+-- load and sync plugins
 require("chris.plugins")
+
+-- custom configurations for each plugin
 require("chris.colorscheme")
 require("chris.cmp")
 require("chris.lsp")
