79250e88d978c3a4a68ce2db4bb36586e32add53
ceva77 <cevanko@binnacle.org>
2023-05-09 14:18:52 -0400

    load gitsigns on opening a file

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,7 +1,7 @@
 return {
     {
         "lewis6991/gitsigns.nvim",
-        lazy = true,
+        event = { "BufReadPre", "BufNewFile" },
         config = function()
             local status_ok, gitsigns = pcall(require, "gitsigns")
             if not status_ok then
