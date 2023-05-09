b3fcf7003f1c7764075f6cc3775d10e16c7d2a61
ceva77 <cevanko@binnacle.org>
2023-05-09 14:22:38 -0400

    Stage current buffer added to whichkey

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -63,11 +63,12 @@ return {
                     g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
                     j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
                     k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
-                    -- l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+                    l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
                     p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
                     r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
                     R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
                     s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+                    S = { "<cmd>lua require 'gitsigns'.stage_buffer()<cr>", "Stage Buffer" },
                     u = {
                         "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
                         "Undo Stage Hunk",
