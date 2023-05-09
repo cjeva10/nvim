2dab922acd0eb903f673b353ae6f61dd977b5935
ceva77 <cevanko@binnacle.org>
2023-05-09 09:44:48 -0400

    Added nvim-surround plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,16 +1,23 @@
 -- this file just loads the leftover plugins that we don't have a custom config for
 return {
+    {
+        "kylechui/nvim-surround",
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({})
+        end,
+    },
     -- comment
     { "numToStr/Comment.nvim", keys = { "<leader>/" } },
 
     -- vim-vyper
-    { "vyperlang/vim-vyper", lazy = true },
+    { "vyperlang/vim-vyper",   lazy = true },
 
     -- alpha greeter
     {
         'goolord/alpha-nvim',
-        config = function ()
-            require'alpha'.setup(require('alpha.themes.dashboard').config)
+        config = function()
+            require 'alpha'.setup(require('alpha.themes.dashboard').config)
         end
     },
 
