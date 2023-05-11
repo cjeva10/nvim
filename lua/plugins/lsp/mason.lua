6fdd6a0e3437a353c907e2f5478a73d09d3839e1
ceva77 <cevanko@binnacle.org>
2023-05-11 10:56:36 -0400

    Fixed bug where lsp's after rust_analyzer were skipped

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -38,15 +38,10 @@ end
 local opts = {}
 
 for _, server in pairs(servers) do
-    -- rust analyzer is handled by rust-tools
-    if server == "rust_analyzer" then
-        return
-    end
     opts = {
         on_attach = require("plugins.lsp.handlers").on_attach,
         capabilities = require("plugins.lsp.handlers").capabilities,
     }
-
     server = vim.split(server, "@")[1]
 
     local require_ok, conf_opts = pcall(require, "plugins.lsp.settings." .. server)
@@ -54,5 +49,9 @@ for _, server in pairs(servers) do
         opts = vim.tbl_deep_extend("force", conf_opts, opts)
     end
 
-    lspconfig[server].setup(opts)
+    if server ~= "rust_analyzer" then
+        lspconfig[server].setup(opts)
+    else
+        print("Skipping rust_analyzer")
+    end
 end
