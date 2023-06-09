955da1cf3de70d733bb03b6ec978fab55d4e0ec1
ceva77 <cevanko@binnacle.org>
2023-06-09 14:29:56 -0400

    add nvim-ts-autotag plugin to treesitter

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -8,29 +8,29 @@ return {
         end,
     },
     -- comment
-    { "numToStr/Comment.nvim", event = { "BufReadPre", "BufNewFile" }, },
+    { "numToStr/Comment.nvim", event = { "BufReadPre", "BufNewFile" } },
 
     -- vim-vyper
-    { "vyperlang/vim-vyper",   lazy = true, ft = "vyper" },
+    { "vyperlang/vim-vyper",   lazy = true,                           ft = "vyper" },
 
     -- alpha greeter
     {
-        'goolord/alpha-nvim',
+        "goolord/alpha-nvim",
         config = function()
-            require 'alpha'.setup(require('alpha.themes.dashboard').config)
-        end
+            require("alpha").setup(require("alpha.themes.dashboard").config)
+        end,
     },
 
     -- R
     { "jalvesaq/Nvim-R", lazy = true, ft = "r" },
 
     -- Better buffer closing actions. Available via the buffers helper.
-	{
-		"kazhala/close-buffers.nvim",
+    {
+        "kazhala/close-buffers.nvim",
         event = { "VeryLazy" },
-		opts = {
-			preserve_window_layout = { "this", "nameless" },
-		},
+        opts = {
+            preserve_window_layout = { "this", "nameless" },
+        },
         config = function()
             -- better buffer deletion
             local buffers = require("helpers.buffers")
@@ -39,45 +39,45 @@ return {
             map("n", "<leader>do", buffers.delete_others, "Other buffers")
             map("n", "<leader>da", buffers.delete_all, "All buffers")
         end,
-	},
+    },
 
     -- autopairs
     {
         "windwp/nvim-autopairs",
         event = { "InsertEnter" },
         config = function()
-            require("nvim-autopairs").setup {}
+            require("nvim-autopairs").setup({})
         end,
     },
     {
         "abecodes/tabout.nvim",
         event = { "InsertEnter" },
         config = function()
-            require('tabout').setup {
-                tabkey = '<C-l>', -- key to trigger tabout, set to an empty string to disable
-                backwards_tabkey = '<C-h>', -- key to trigger backwards tabout, set to an empty string to disable
+            require("tabout").setup({
+                tabkey = "<C-l>", -- key to trigger tabout, set to an empty string to disable
+                backwards_tabkey = "<C-h>", -- key to trigger backwards tabout, set to an empty string to disable
                 act_as_tab = false, -- shift content if tab out is not possible
                 act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-                default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-                default_shift_tab = '<C-d>', -- reverse shift default action,
+                default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+                default_shift_tab = "<C-d>", -- reverse shift default action,
                 enable_backwards = true, -- well ...
                 completion = false, -- if the tabkey is used in a completion pum
                 tabouts = {
-                  {open = "'", close = "'"},
-                  {open = '"', close = '"'},
-                  {open = '`', close = '`'},
-                  {open = '(', close = ')'},
-                  {open = '[', close = ']'},
-                  {open = '{', close = '}'}
+                    { open = "'", close = "'" },
+                    { open = '"', close = '"' },
+                    { open = "`", close = "`" },
+                    { open = "(", close = ")" },
+                    { open = "[", close = "]" },
+                    { open = "{", close = "}" },
+                    { open = "<", close = ">" },
                 },
                 ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
-            }
+            })
         end,
-        dependencies =
-            {
-                "nvim-treesitter/nvim-treesitter",
-                "hrsh7th/nvim-cmp",
-            },
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "hrsh7th/nvim-cmp",
+        },
     },
 
     -- show insert blanklines
@@ -90,6 +90,13 @@ return {
             filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
             show_trailing_blankline_indent = false,
             show_current_context = false,
+        },
+    },
+
+    {
+        "windwp/nvim-ts-autotag",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
         },
     },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,43 +1,45 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        event = { "BufReadPost", "BufNewFile" },
-        config = function()
-            -- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-            -- parser_config.move = {
-            --     install_info = {
-            --         url = "/home/chris/coding/move/tree-sitter-move",
-            --         files = { "src/parser.c" },
-            --     },
-            --     filetype = "move",
-            -- }
-            require("nvim-treesitter.configs").setup {
-              ensure_installed = {
-                "c",
-                "cpp",
-                "go",
-                "javascript",
-                "lua",
-                -- "move",
-                "python",
-                "rust",
-                "solidity",
-                "toml",
-                "typescript",
-              },
-              sync_install = false,
-              ignore_install = { "" }, -- List of parsers to ignore installing
-              highlight = {
-                enable = true, -- false will disable the whole extension
-                disable = { "" }, -- list of language that will be disabled
-                additional_vim_regex_highlighting = false,
-
-              },
-              indent = { enable = true, disable = { "yaml" } },
-            }
-        end,
-        build = function()
-            pcall(require("nvim-treesitter.install").update({ with_sync = true}))
-        end,
-    }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			-- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			-- parser_config.move = {
+			--     install_info = {
+			--         url = "/home/chris/coding/move/tree-sitter-move",
+			--         files = { "src/parser.c" },
+			--     },
+			--     filetype = "move",
+			-- }
+			require("nvim-treesitter.configs").setup({
+				autotag = {
+					enable = true,
+				},
+				ensure_installed = {
+					"c",
+					"cpp",
+					"go",
+					"javascript",
+					"lua",
+					-- "move",
+					"python",
+					"rust",
+					"solidity",
+					"toml",
+					"typescript",
+				},
+				sync_install = false,
+				ignore_install = { "" }, -- List of parsers to ignore installing
+				highlight = {
+					enable = true, -- false will disable the whole extension
+					disable = { "" }, -- list of language that will be disabled
+					additional_vim_regex_highlighting = false,
+				},
+				indent = { enable = true, disable = { "yaml" } },
+			})
+		end,
+		build = function()
+			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
+		end,
+	},
 }
