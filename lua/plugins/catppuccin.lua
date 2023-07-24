a8df3f231d850e8eafd6bbe0d9311b0163a81aff
ceva77 <cevanko@binnacle.org>
2023-07-24 04:22:57 -0400

    added integrations to catppuccin plugin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,7 +5,8 @@ local M = {
     priority = 1000,
     -- setup must be called before loading
     config = function()
-        require("catppuccin").setup({
+        local catppuccin = require("catppuccin")
+        catppuccin.setup({
             flavour = "mocha", -- latte, frappe, macchiato, mocha
             background = { -- :h background
                 light = "latte",
@@ -41,12 +42,14 @@ local M = {
                 gitsigns = true,
                 nvimtree = true,
                 telescope = true,
+                treesitter = true,
+                mason = true,
                 notify = false,
-                mini = false,
+                mini = true,
                 -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
             },
         })
-        vim.cmd([[colorscheme catppuccin]])
+        catppuccin.load()
     end,
 }
 
