3cc6a3a63980cec7f0e9ff0baafdcbe674eb3f6a
cjeva10 <cjevanko@gmail.com>
2023-11-09 15:31:10 -0500

    add ruff to null-ls

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,101 +1,101 @@
 -- this file just loads the leftover plugins that we don't have a custom config for
 return {
-	{
-		"kylechui/nvim-surround",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			require("nvim-surround").setup({})
-		end,
-	},
-	-- comment
-	{ "numToStr/Comment.nvim", event = { "BufReadPre", "BufNewFile" } },
+    {
+        "kylechui/nvim-surround",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("nvim-surround").setup({})
+        end,
+    },
+    -- comment
+    { "numToStr/Comment.nvim", event = { "BufReadPre", "BufNewFile" } },
 
-	-- vim-vyper
-	{ "vyperlang/vim-vyper", lazy = true, ft = "vyper" },
+    -- vim-vyper
+    { "vyperlang/vim-vyper",   lazy = true,                           ft = "vyper" },
 
-	-- alpha greeter
-	-- {
-	--     "goolord/alpha-nvim",
-	--     config = function()
-	--         require("alpha").setup(require("alpha.themes.dashboard").config)
-	--     end,
-	-- },
+    -- alpha greeter
+    -- {
+    --     "goolord/alpha-nvim",
+    --     config = function()
+    --         require("alpha").setup(require("alpha.themes.dashboard").config)
+    --     end,
+    -- },
 
-	-- R
-	{ "jalvesaq/Nvim-R", lazy = true, ft = "r" },
+    -- R
+    { "jalvesaq/Nvim-R",       lazy = true,                           ft = "r" },
 
-	-- Better buffer closing actions. Available via the buffers helper.
-	{
-		"kazhala/close-buffers.nvim",
-		event = { "VeryLazy" },
-		opts = {
-			preserve_window_layout = { "this", "nameless" },
-		},
-		config = function()
-			-- better buffer deletion
-			local buffers = require("helpers.buffers")
-			local map = require("helpers.keys").map
-			map("n", "<leader>db", buffers.delete_this, "Current buffer")
-			map("n", "<leader>do", buffers.delete_others, "Other buffers")
-			map("n", "<leader>da", buffers.delete_all, "All buffers")
-		end,
-	},
+    -- Better buffer closing actions. Available via the buffers helper.
+    {
+        "kazhala/close-buffers.nvim",
+        event = { "VeryLazy" },
+        opts = {
+            preserve_window_layout = { "this", "nameless" },
+        },
+        config = function()
+            -- better buffer deletion
+            local buffers = require("helpers.buffers")
+            local map = require("helpers.keys").map
+            map("n", "<leader>db", buffers.delete_this, "Current buffer")
+            map("n", "<leader>do", buffers.delete_others, "Other buffers")
+            map("n", "<leader>da", buffers.delete_all, "All buffers")
+        end,
+    },
 
-	-- autopairs
-	{
-		"windwp/nvim-autopairs",
-		event = { "InsertEnter" },
-		config = function()
-			require("nvim-autopairs").setup({})
-		end,
-	},
-	{
-		"windwp/nvim-ts-autotag",
-		event = { "BufReadPre *.html", "BufNewFile *.html", "BufNewFile *.css", "BufReadPre *.css" },
-	},
-	{
-		"abecodes/tabout.nvim",
-		event = { "InsertEnter" },
-		config = function()
-			require("tabout").setup({
-				tabkey = "<C-l>", -- key to trigger tabout, set to an empty string to disable
-				backwards_tabkey = "<C-h>", -- key to trigger backwards tabout, set to an empty string to disable
-				act_as_tab = false, -- shift content if tab out is not possible
-				act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-				default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-				default_shift_tab = "<C-d>", -- reverse shift default action,
-				enable_backwards = true, -- well ...
-				completion = false, -- if the tabkey is used in a completion pum
-				tabouts = {
-					{ open = "'", close = "'" },
-					{ open = '"', close = '"' },
-					{ open = "`", close = "`" },
-					{ open = "(", close = ")" },
-					{ open = "[", close = "]" },
-					{ open = "{", close = "}" },
-					{ open = "<", close = ">" },
-				},
-				ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
-			})
-		end,
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"hrsh7th/nvim-cmp",
-		},
-	},
+    -- autopairs
+    {
+        "windwp/nvim-autopairs",
+        event = { "InsertEnter" },
+        config = function()
+            require("nvim-autopairs").setup({})
+        end,
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        event = { "BufReadPre *.html", "BufNewFile *.html", "BufNewFile *.css", "BufReadPre *.css" },
+    },
+    {
+        "abecodes/tabout.nvim",
+        event = { "InsertEnter" },
+        config = function()
+            require("tabout").setup({
+                tabkey = "<C-l>",            -- key to trigger tabout, set to an empty string to disable
+                backwards_tabkey = "<C-h>",  -- key to trigger backwards tabout, set to an empty string to disable
+                act_as_tab = false,          -- shift content if tab out is not possible
+                act_as_shift_tab = false,    -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+                default_tab = "<C-t>",       -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+                default_shift_tab = "<C-d>", -- reverse shift default action,
+                enable_backwards = true,     -- well ...
+                completion = false,          -- if the tabkey is used in a completion pum
+                tabouts = {
+                    { open = "'", close = "'" },
+                    { open = '"', close = '"' },
+                    { open = "`", close = "`" },
+                    { open = "(", close = ")" },
+                    { open = "[", close = "]" },
+                    { open = "{", close = "}" },
+                    { open = "<", close = ">" },
+                },
+                ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+            })
+        end,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "hrsh7th/nvim-cmp",
+        },
+    },
 
-	-- show insert blanklines
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		-- opts = {
-		-- 	-- char = "▏",
-		-- 	char = "│",
-		-- 	filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
-		-- 	show_trailing_blankline_indent = false,
-		-- 	show_current_context = false,
-		-- },
-	},
+    -- show insert blanklines
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        -- opts = {
+        -- 	-- char = "▏",
+        -- 	char = "│",
+        -- 	filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+        -- 	show_trailing_blankline_indent = false,
+        -- 	show_current_context = false,
+        -- },
+    },
 
     -- distant for using on personal vps
     {
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,19 +1,22 @@
 return {
-	{
-		"jose-elias-alvarez/null-ls.nvim",
+    {
+        "jose-elias-alvarez/null-ls.nvim",
         event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.black,
-					null_ls.builtins.formatting.isort,
-				},
-			})
-		end,
-	},
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            local null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.black,
+                    null_ls.builtins.formatting.isort,
+                    -- null_ls.builtins.diagnostics.flake8.with({ extra_args = { "--max-line-length", "88" } }),
+                    null_ls.builtins.diagnostics.ruff.with({ extra_args = { "--max-line-length", "88" } }),
+                    -- null_ls.builtins.diagnostics.mypy,
+                },
+            })
+        end,
+    },
 }
