7c86aeb8ee16b8556fc35ef4af0318beae1d5fd5
cjeva10 <cjevanko@gmail.com>
2024-05-13 15:06:58 -0400

    add ruff formatting for python

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -10,9 +10,8 @@ return {
             null_ls.setup({
                 sources = {
                     null_ls.builtins.formatting.stylua,
-                    null_ls.builtins.formatting.black,
-                    null_ls.builtins.formatting.isort,
-                    -- null_ls.builtins.diagnostics.flake8.with({ extra_args = { "--max-line-length", "88" } }),
+                    -- ruff for python
+                    null_ls.builtins.formatting.ruff,
                     null_ls.builtins.diagnostics.ruff.with({ extra_args = { "--max-line-length", "88" } }),
                     -- null_ls.builtins.diagnostics.mypy,
                 },
