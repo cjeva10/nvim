8f5a54293d05de4b8dee843f3bd602cd8bfe6440
cjeva10 <cjevanko@gmail.com>
2024-05-15 17:21:25 -0400

    remove extra line length arg for ruff

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,20 +1,20 @@
 return {
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.ruff,
-					null_ls.builtins.diagnostics.ruff.with({ extra_args = { "--max-line-length", "88" } }),
-					-- null_ls.builtins.diagnostics.mypy,
-				},
-			})
-		end,
-	},
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            local null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.ruff,
+                    null_ls.builtins.diagnostics.ruff,
+                    -- null_ls.builtins.diagnostics.mypy,
+                },
+            })
+        end,
+    },
 }
