ada6322914292e4f071bb5e532911b6bb0446704
cjeva10 <cjevanko@gmail.com>
2024-06-11 15:09:28 -0400

    rename catppuccin file to colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,68 @@
+local C = {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = false,
+	priority = 1000,
+	-- setup must be called before loading
+	config = function()
+		local catppuccin = require("catppuccin")
+		catppuccin.setup({
+			flavour = "mocha", -- latte, frappe, macchiato, mocha
+			background = { -- :h background
+				light = "latte",
+				dark = "mocha",
+			},
+			transparent_background = true,
+			term_colors = true,
+			dim_inactive = {
+				enabled = false,
+				shade = "dark",
+				percentage = 0.15,
+			},
+			no_italic = false, -- Force no italic
+			no_bold = false, -- Force no bold
+			styles = {
+				comments = { "italic" },
+				conditionals = { "italic" },
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+				operators = {},
+			},
+			color_overrides = {},
+			custom_highlights = {},
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				telescope = true,
+				treesitter = true,
+				mason = true,
+				notify = false,
+				mini = true,
+				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+			},
+		})
+		catppuccin.load()
+	end,
+}
+
+local V = {
+	"Mofiqul/vscode.nvim",
+	name = "vscode",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		local vscode = require("vscode")
+		vscode.setup({})
+		vim.cmd("colorscheme vscode")
+	end,
+}
+
+return { C }
