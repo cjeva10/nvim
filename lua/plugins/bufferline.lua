c0672215e18e3ffc2b731c6b3206ed044c86af8d
cjeva10 <cjevanko@gmail.com>
2024-06-29 20:53:26 -0400

    remove extra rose-pine config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,18 +1,17 @@
 return {
-    {
-        "akinsho/bufferline.nvim",
-        event = "VeryLazy",
-        dependencies = {
-            "moll/vim-bbye",
-        },
-        config = function()
-            local status_ok, bufferline = pcall(require, "bufferline")
-            if not status_ok then
-                return
-            end
+	{
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"moll/vim-bbye",
+		},
+		config = function()
+			local status_ok, bufferline = pcall(require, "bufferline")
+			if not status_ok then
+				return
+			end
 
-            local highlights = require("rose-pine.plugins.bufferline")
-            bufferline.setup({ highlights = highlights })
-        end,
-    },
+			bufferline.setup({})
+		end,
+	},
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,14 +1,12 @@
 return {
-    {
-        "nvim-lualine/lualine.nvim",
-        event = "VeryLazy",
-        dependencies = {
-            "AndreM222/copilot-lualine",
-        },
-        config = function()
-            require("lualine").setup({
-                theme = "rose-pine",
-            })
-        end,
-    },
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"AndreM222/copilot-lualine",
+		},
+		config = function()
+			require("lualine").setup({})
+		end,
+	},
 }
