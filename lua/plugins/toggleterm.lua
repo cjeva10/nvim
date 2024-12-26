443626b0a8c5e2a9a745c0f130f8706705d262fb
cjeva10 <cjevanko@gmail.com>
2024-12-26 14:06:37 -0500

    simplify toggleterm options

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,8 +1,6 @@
 return {
     {
         "akinsho/toggleterm.nvim",
-        cmd = "ToggleTerm",
-        keys = { "<C-t>", "<leader>" },
         config = function()
             local status_ok, toggleterm = pcall(require, "toggleterm")
             if not status_ok then
@@ -10,22 +8,10 @@ return {
             end
 
             toggleterm.setup({
-                size = 120,
-                open_mapping = [[<c-t>]],
-                hide_numbers = true,
-                shade_filetypes = {},
-                shade_terminals = true,
+                size = 80,
+                open_mapping = [[<C-t>]],
                 shading_factor = 2,
-                start_in_insert = true,
-                insert_mappings = true,
-                persist_size = true,
                 direction = "vertical",
-                close_on_exit = true,
-                shell = vim.o.shell,
-                float_opts = {
-                    border = "curved",
-                    winblend = 0,
-                },
             })
 
             function _G.set_terminal_keymaps()
@@ -37,10 +23,6 @@ return {
             end
 
             vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
-
-            -- bind different terminal functions
-            local map = require("helpers.keys").map
-            map("n", "<leader>t", "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical")
         end,
     },
 }
