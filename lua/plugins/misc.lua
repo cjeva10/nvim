c7ca355321ad15dc37ceb6a19c7f791dba5e3b0a
cjeva10 <cjevanko@gmail.com>
2024-12-26 10:09:21 -0500

    add leap.nvim

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -32,4 +32,15 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         opts = {},
     },
+    {
+        "ggandor/leap.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+		    local map = require("helpers.keys").map
+            map('n',        's', '<Plug>(leap)')
+            map('n',        'S', '<Plug>(leap-from-window)')
+            map({'x', 'o'}, 's', '<Plug>(leap-forward)')
+            map({'x', 'o'}, 'S', '<Plug>(leap-backward)')
+        end,
+    }
 }
