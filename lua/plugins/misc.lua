5102676291c29c3610599c9cc9f03783af493bb3
cjeva10 <cjevanko@gmail.com>
2025-01-03 10:19:12 -0500

    fix comment api keymap in visual mode

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -14,7 +14,7 @@ return {
             local comment = require("Comment.api")
             local map = require("helpers.keys").map
             map("n", "<leader>c", comment.toggle.linewise.current, "Comment")
-            map("v", "<leader>c", comment.toggle.linewise.current, "Comment")
+            map("v" , "<leader>c", '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>', "Comment visual")
         end,
     },
     { "vyperlang/vim-vyper", lazy = true, ft = "vyper" },
