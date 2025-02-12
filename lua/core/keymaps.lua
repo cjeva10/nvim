4d9b21712575d601ff873b9aef79b0c5b26fe748
cjeva10 <cjevanko@gmail.com>
2025-02-11 20:51:04 -0500

    simplify keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -11,22 +11,8 @@ set_leader(" ")
 --   term_mode = "t",
 --   command_mode = "c",
 
--- Normal --
--- Better window navigation
-map("n", "<C-h>", "<C-w>h")
-map("n", "<C-j>", "<C-w>j")
-map("n", "<C-k>", "<C-w>k")
-map("n", "<C-l>", "<C-w>l")
-
-map("n", "<leader>wh", "<C-w>h")
-map("n", "<leader>wj", "<C-w>j")
-map("n", "<leader>wk", "<C-w>k")
-map("n", "<leader>wl", "<C-w>l")
-
-map("n", "<leader>wv", ":vsplit<CR>")
-map("n", "<leader>ws", ":split<CR>")
-map("n", "<leader>w ", ":w<CR>")
-map("n", "<leader>wq", ":q<CR>")
+-- use spc-w to edit windows
+map("n", "<leader>w", "<C-w>")
 
 -- helix-like g motions
 map("n", "gh", "0")
@@ -37,46 +23,15 @@ map("n", "ge", "G")
 -- Redo
 map("n", "U", "<C-r>")
 
--- Resize with arrows
-map("n", "<C-Up>", ":resize -2<CR>")
-map("n", "<C-Down>", ":resize +2<CR>")
-map("n", "<C-Left>", ":vertical resize -2<CR>")
-map("n", "<C-Right>", ":vertical resize +2<CR>")
-
 -- Navigate buffers
 map("n", "<S-l>", ":bnext<CR>")
 map("n", "<S-h>", ":bprevious<CR>")
+
+-- Save and close buffer
 map("n", "<leader>wc", ":bdelete<CR>")
-
--- Move text up and down
-map("n", "<A-j>", "<Esc>:m .+1<CR>==gi")
-map("n", "<A-k>", "<Esc>:m .-2<CR>==gi")
-
--- Insert --
--- Press jk fast to exit insert mode 
-map("i", "jk", "<ESC>")
-map("i", "kj", "<ESC>")
+map("n", "<leader>w ", ":w<CR>")
 
 -- Visual --
 -- Stay in indent mode when moving indents
 map("v", "<", "<gv")
-map("v", "<", "<gv")
-
--- Move text up and down
-map("v", "<A-j>", ":m .+1<CR>==")
-map("v", "<A-k>", ":m .-2<CR>==")
-map("v", "p", '"_dP')
-
--- Visual Block --
--- Move text up and down
-map("x", "J", ":move '>+1<CR>gv-gv")
-map("x", "K", ":move '<-2<CR>gv-gv")
-map("x", "<A-j>", ":move '>+1<CR>gv-gv")
-map("x", "<A-k>", ":move '<-2<CR>gv-gv")
-
--- Terminal --
--- Better terminal navigation
-map("t", "<C-h>", "<C-\\><C-N><C-w>h")
-map("t", "<C-j>", "<C-\\><C-N><C-w>j")
-map("t", "<C-k>", "<C-\\><C-N><C-w>k")
-map("t", "<C-l>", "<C-\\><C-N><C-w>l")
+map("v", ">", ">gv")
