6c285f873b80f5d47aa52b2cd234a58c947836a2
cjeva10 <cjevanko@gmail.com>
2025-10-09 16:20:23 -0400

    remove unneeded keybinds in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -13,22 +13,11 @@ return {
 				return
 			end
 
-			local actions = require("telescope.actions")
-
 			telescope.setup({
 				defaults = {
 					prompt_prefix = " ",
 					selection_caret = " ",
 					path_display = { "smart" },
-
-					mappings = {
-						i = {
-							["<C-u>"] = actions.preview_scrolling_up,
-							["<C-d>"] = actions.preview_scrolling_down,
-							["<C-k>"] = actions.move_selection_previous,
-							["<C-j>"] = actions.move_selection_next,
-						},
-					},
 				},
 
 				extensions = {
