0feaf7d1188f85ba8bb73194adb446fe2c2cd702
cjeva10 <cjevanko@gmail.com>
2025-10-12 22:10:30 -0400

    use C-n/p for selecting auto-complete entries

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -69,8 +69,8 @@ local M = {
                 end,
             },
             mapping = {
-                ["<C-k>"] = cmp.mapping.select_prev_item(),
-                ["<C-j>"] = cmp.mapping.select_next_item(),
+                ["<C-p>"] = cmp.mapping.select_prev_item(),
+                ["<C-n>"] = cmp.mapping.select_next_item(),
                 ["<C-e>"] = cmp.mapping.abort(),
                 ["<CR>"] = cmp.mapping.confirm(),
                 ["<C-Space>"] = cmp.mapping.complete(),
