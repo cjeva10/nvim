ca9c12ccf1a3881551c18702af556c0664558e92
cjeva10 <cjevanko@gmail.com>
2025-10-28 11:43:20 -0400

    remove lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,16 +1,16 @@
 return {
-	{
-		"nvim-lualine/lualine.nvim",
-		event = "VeryLazy",
-		dependencies = {
-			"AndreM222/copilot-lualine",
-		},
-		config = function()
-			require("lualine").setup({
-                options = {
-                    theme = "catppuccin"
-                }
-            })
-		end,
-	},
+	-- {
+	-- 	"nvim-lualine/lualine.nvim",
+	-- 	event = "VeryLazy",
+	-- 	dependencies = {
+	-- 		"AndreM222/copilot-lualine",
+	-- 	},
+	-- 	config = function()
+	-- 		require("lualine").setup({
+ --                options = {
+ --                    theme = "catppuccin"
+ --                }
+ --            })
+	-- 	end,
+	-- },
 }
