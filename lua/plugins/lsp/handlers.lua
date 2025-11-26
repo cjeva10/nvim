90ae6de298620af637f070a30ddfa44198658439
cjeva10 <cjevanko@gmail.com>
2025-11-26 09:43:24 -0500

    delete autocmds for document highlight - add manual keybinds instead

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -1,21 +1,5 @@
 local M = {}
 
-local function lsp_highlight_document(client)
-    -- Set autocommands conditional on server_capabilities
-    if client.server_capabilities.documentHighlight then
-        vim.api.nvim_exec2(
-            [[
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]],
-            {}
-        )
-    end
-end
-
 local function lsp_keymaps(bufnr)
     local lsp_map = require("helpers.keys").lsp_map
     local tb = require("telescope.builtin")
@@ -38,11 +22,12 @@ local function lsp_keymaps(bufnr)
     lsp_map("<leader>lr", "<cmd>LspRestart<cr>", bufnr, "Lsp Restart")
     lsp_map("<leader>ls", tb.lsp_dynamic_workspace_symbols, bufnr, "Workspace Symbols")
     lsp_map("<leader>lt", vim.lsp.buf.type_definition, bufnr, "Type definition")
+    lsp_map("<leader>lh", vim.lsp.buf.document_highlight, bufnr, "Highlight document")
+    lsp_map("<leader>lc", vim.lsp.buf.clear_references, bufnr, "Clear All References")
 end
 
-M.on_attach = function(client, bufnr)
+M.on_attach = function(_, bufnr)
     lsp_keymaps(bufnr)
-    lsp_highlight_document(client)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
