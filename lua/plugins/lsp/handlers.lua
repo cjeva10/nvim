a166f002de357ffe34a4139d6d49c21d39bb6cfb
cjeva10 <cjevanko@gmail.com>
2025-11-26 09:15:22 -0500

    fix deprecated nvim_exec

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -3,7 +3,7 @@ local M = {}
 local function lsp_highlight_document(client)
     -- Set autocommands conditional on server_capabilities
     if client.server_capabilities.documentHighlight then
-        vim.api.nvim_exec(
+        vim.api.nvim_exec2(
             [[
       augroup lsp_document_highlight
         autocmd! * <buffer>
@@ -11,7 +11,7 @@ local function lsp_highlight_document(client)
         autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
       augroup END
     ]],
-            false
+            {}
         )
     end
 end
