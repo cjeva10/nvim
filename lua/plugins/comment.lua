2cf0fa51d4a1ac3514107d2349fee5e1dbc3ade1
cjeva10 <cjevanko@gmail.com>
2025-11-26 22:07:51 -0500

    move misc plugins into their own files

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "numToStr/Comment.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            local comment = require("Comment.api")
+            local map = require("helpers.keys").map
+            map("n", "<leader>c", comment.toggle.linewise.current, "Comment")
+            map("v", "<leader>c", '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>',
+                "Comment visual")
+        end,
+    },
+}
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,20 +1,4 @@
--- this file just loads the leftover plugins that we don't have a custom config for
 return {
-    {
-        "numToStr/Comment.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            local comment = require("Comment.api")
-            local map = require("helpers.keys").map
-            map("n", "<leader>c", comment.toggle.linewise.current, "Comment")
-            map("v", "<leader>c", '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>',
-                "Comment visual")
-        end,
-    },
-    {
-        "lewis6991/gitsigns.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-    },
     {
         "stevearc/oil.nvim",
         dependencies = {
