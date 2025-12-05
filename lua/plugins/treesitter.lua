fea4edeba2f42e0568a9453d2a9c5da92da6028c
cjeva10 <cjevanko@gmail.com>
2025-12-05 12:26:45 -0500

    use main branch for treesitter now

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,55 +3,33 @@ return {
         "nvim-treesitter/nvim-treesitter",
         branch = 'main',
         lazy = false,
-        dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+        build = ":TSUpdate",
         config = function()
-            local treesitter_configs = require("nvim-treesitter.configs")
+            local filetypes = {
+                "c",
+                "cpp",
+                "go",
+                "html",
+                "htmldjango",
+                "javascript",
+                "java",
+                "lua",
+                "python",
+                "ruby",
+                "rust",
+                "solidity",
+                "toml",
+                "typescript",
+            }
 
-            treesitter_configs.setup({
-                ensure_installed = {
-                    "c",
-                    "cpp",
-                    "go",
-                    "html",
-                    "htmldjango",
-                    "javascript",
-                    "java",
-                    "lua",
-                    "python",
-                    "ruby",
-                    "rust",
-                    "solidity",
-                    "toml",
-                    "typescript",
-                },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
-                            ["as"] = "@local.scope",
-                        },
-                    },
-                },
-                autotag = {
-                    enable = true,
-                },
-                sync_install = false,
-                ignore_install = { "" }, -- list of parsers to ignore installing
-                highlight = {
-                    enable = true,       -- false will disable the whole extension
-                    disable = { "" },    -- list of language that will be disabled
-                    additional_vim_regex_highlighting = false,
-                },
-                indent = { enable = true, disable = { "yaml" } },
+            require("nvim-treesitter").install(filetypes)
+
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = filetypes,
+                callback = function()
+                    vim.treesitter.start()
+                end,
             })
-        end,
-        build = function()
-            require("nvim-treesitter.install").update({ with_sync = true })
         end,
     },
 }
